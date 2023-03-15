b265193dac399a9c8477ea6aa21160d131da8cff
TripVerse <122679016+Tripverseofficial@users.noreply.github.com>
2023-03-15 15:08:51 +0700

    Update test.py
    This code is testing a smart contract called Trip. The Trip contract has several functions that can be called to interact with it.
    
    The first part of the test code deploys the contract using a specific Ethereum address (0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419) as a parameter and the first account in the local Ganache instance (accounts[0]) as the deployer.
    
    The following assertions check the initial state of the contract to ensure it was properly deployed. These assertions check values such as basePrice, lastPrice, nextRebaseTime, decimals, and maxSupply.
    
    Next, the getPrice() function is tested to ensure that it returns the expected result. The updateBasePrice() function is then called and tested to ensure that the basePrice of the contract has been updated.
    
    The calculateRebaseInterval() function is then tested to ensure that it returns one of the expected values.
    
    Finally, the rebase() function is tested. This function is expected to fail and raise an error message because the maximum supply of the contract has been reached. The totalSupply() function is then called to ensure that the total supply of the contract hasn't changed. The nextRebaseTime() function is also checked to ensure it has been properly updated after the rebase attempt.

--- test/test.py
+++ test/test.py
@@ -1,9 +1,36 @@
-from brownie import Trip, network
-import pytest
+import brownie
+from brownie import Trip, accounts
 
-@pytest.fixture
-def trip():
-    return Trip.deploy({'from': get_account()})
+def test_trip_contract():
+    # Deploy the contract
+    trip = Trip.deploy("0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419", {'from': accounts[0]})
 
-def test_initial_supply(trip):
-    assert trip.totalSupply() == 1000000 * 10 ** 18
+    # Check that the contract deployed successfully
+    assert trip is not None
+
+    # Check the initial state of the contract
+    assert trip.basePrice() == 0.01 * 10 ** 18
+    assert trip.lastPrice() == 0.01 * 10 ** 18
+    assert trip.nextRebaseTime() == trip.block_timestamp() + 1 * 24 * 60 * 60
+    assert trip.decimals() == 18
+    assert trip.maxSupply() == 100000000 * 10 ** 18
+
+    # Test the getPrice() function
+    assert trip.getPrice() == trip.basePrice() * trip.lastPrice() // (1 * 10 ** 18)
+
+    # Test the updateBasePrice() function
+    trip.updateBasePrice({'from': accounts[0]})
+    assert trip.basePrice() != 0.01 * 10 ** 18
+
+    # Test the calculateRebaseInterval() function
+    rebase_interval = trip.calculateRebaseInterval()
+    assert rebase_interval == 24 * 60 * 60 or \
+           rebase_interval == 12 * 60 * 60 or \
+           rebase_interval == 6 * 60 * 60 or \
+           rebase_interval == 1 * 60 * 60
+
+    # Test the rebase() function
+    with brownie.reverts("Max supply reached"):
+        trip.rebase({'from': accounts[0]})
+    assert trip.totalSupply() == 100000000 * 10 ** 18
+    assert trip.nextRebaseTime() == trip.block_timestamp() + 1 * 24 * 60 * 60
