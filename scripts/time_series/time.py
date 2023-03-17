71c6792ebbe7b951b60be1b418af1e7691c17651
TripVerse <122679016+Tripverseofficial@users.noreply.github.com>
2023-03-17 09:53:44 +0700

    Update time.py

--- scripts/time_series/time.py
+++ scripts/time_series/time.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import numpy as np
-from statsmodels.tsa.arima.model import ARIMA
 from concurrent.futures import ProcessPoolExecutor
-from new_scrapper import scrape_news
-from twitter_scrapper import scrape_tweets
-from preprocessing import preprocess_data
-from sentiment_analysis import analyze_sentiment
-
 from data.load_data import load_historical_data, load_market_data
+from preprocessing import preprocess_data, prepare_data
+from sentiment_analysis import analyze_sentiment, train_sentiment_analysis_model
+from sklearn.preprocessing import MinMaxScaler
+from statsmodels.tsa.arima.model import ARIMA
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_absolute_error
 
+def train_lstm(X_train, y_train, X_test, y_test):
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(1, X_train.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=0)
+    y_pred = model.predict(X_test)
+    return mean_absolute_error(y_test, y_pred)
+
+# Load historical data and market data
 df = load_historical_data()
 market_df = load_market_data()
 
@@ -18,49 +30,71 @@ df['timestamp'] = pd.to_datetime(df['timestamp'])
 # Set timestamp as index
 df.set_index('timestamp', inplace=True)
 
-# Prepare data for ARIMA model
-X = df['price'].values
-size = int(len(X) * 0.8)
-train, test = X[0:size], X[size:len(X)]
-history = [x for x in train]
+# Add more features to the dataset
+df = prepare_data(df, market_df)
+
+# Prepare data for time series forecasting
+scaler = MinMaxScaler(feature_range=(0, 1))
+X = scaler.fit_transform(df.drop(columns=['price']).values)
+y = X[:, 0]
+n_splits = 5
+tscv = TimeSeriesSplit(n_splits=n_splits)
+
+# Train and evaluate LSTM model
+mae_scores = []
+with ProcessPoolExecutor() as executor:
+    for train_index, test_index in tscv.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]-1))
+        X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]-1))
+        mae_scores.append(train_lstm(X_train, y_train, X_test, y_test))
+print('LSTM MAE:', np.mean(mae_scores))
 
 # Fit ARIMA model to historical data
-model = ARIMA(history, order=(5,1,0))
+model = ARIMA(y, order=(5,1,0))
 model_fit = model.fit()
 
 # Make ARIMA predictions
-with ProcessPoolExecutor() as executor:
-    arima_predictions = list(executor.map(lambda x: x[0], [model_fit.forecast() for _ in range(len(test))]))
-history.extend(test)
+size = int(len(y) * 0.2)
+arima_predictions = model_fit.forecast(steps=len(y)-size)[0]
 
-# Market Sentiment Analysis
-# Train market sentiment model
-market_sentiment_model = train_market_sentiment_model() # replace with your function to train market sentiment model
-
-# Make market sentiment predictions
-market_sentiment_predictions = market_sentiment_model.predict(market_df) # replace with your function to predict market sentiment
-
-# Make sentiment impact predictions
-sentiment_impact_predictions = predict_sentiment_impact(market_sentiment_predictions, combined_sentiment_scores) # replace with your function to predict the impact of market sentiment on token price
-
-
-# Scrape news articles and tweets
+# Scrape news articles, tweets and telegram messages
 news_df = scrape_news()
 tweets_df = scrape_tweets()
+telegram_df = scrape_telegram()
 
 # Preprocess data
 with ProcessPoolExecutor() as executor:
-    clean_news_df, clean_tweets_df = list(executor.map(preprocess_data, [news_df, tweets_df]))
+    clean_news_df, clean_tweets_df, clean_telegram_df = list(executor.map(preprocess_data, [news_df, tweets_df, telegram_df]))
 
 # Perform sentiment analysis
 with ProcessPoolExecutor() as executor:
-    news_sentiment_scores, tweets_sentiment_scores = list(executor.map(analyze_sentiment, [clean_news_df, clean_tweets_df]))
+    news_sentiment_scores, tweets_sentiment_scores, telegram_sentiment_scores = list(executor.map(analyze_sentiment, [clean_news_df, clean_tweets_df, clean_telegram_df]))
 
 # Combine sentiment scores
-combined_sentiment_scores = np.concatenate((news_sentiment_scores, tweets_sentiment_scores))
-                                            
-# Combine ARIMA and market sentiment predictions
-combined_predictions = np.add(arima_predictions, sentiment_impact_predictions)
+combined_sentiment_scores = np.concatenate((news_sentiment_scores, tweets_sentiment_scores, telegram_sentiment_scores))
 
-# Print predictions
-print(combined_predictions)
+# Train and evaluate sentiment analysis model
+model = train_sentiment_analysis_model(combined_sentiment_scores) 
+sentiment_scores = model.predict(combined_sentiment_scores)
+mae_scores = []
+for train_index, test_index in tscv.split(sentiment_scores):
+    y_train, y_test = y[train_index], y[test_index]
+    y_pred = sentiment_scores[test_index]
+    mae = mean_absolute_error(y_test, y_pred)
+    mae_scores.append(mae)
+    print('Sentiment Analysis MAE:', np.mean(mae_scores))
+
+    # Combine LSTM and ARIMA predictions with sentiment analysis
+    combined_predictions = (arima_predictions + sentiment_scores) / 2
+
+    # Scale predictions back to original range
+    combined_predictions = scaler.inverse_transform(np.concatenate(([combined_predictions[0]], combined_predictions[1:])))
+    arima_predictions = scaler.inverse_transform(np.concatenate(([arima_predictions[0]], arima_predictions[1:])))
+
+    # Output predictions
+    print('ARIMA Predictions:', arima_predictions[-1])
+    print('LSTM + Sentiment Analysis Predictions:', combined_predictions[-1])
+    
+
