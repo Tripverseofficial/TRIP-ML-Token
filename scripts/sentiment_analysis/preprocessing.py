e8d2d7b5690c2bf8c2c1f02215424237534796c3
TripVerse <122679016+Tripverseofficial@users.noreply.github.com>
2023-03-21 22:49:40 +0700

    Update preprocessing.py
    Downloaded additional NLTK resources: I added the averaged_perceptron_tagger corpus to enable part-of-speech tagging and the wordnet corpus to enable lemmatization.
    
    Customized preprocessing steps: I added an optional language argument to the preprocess_text function, which can be used to specify the language of the input text

--- scripts/sentiment_analysis/preprocessing.py
+++ scripts/sentiment_analysis/preprocessing.py
@@ -1,30 +1,52 @@
 import re
+import spacy
 import nltk
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-from data.sentiments_analysis.preprocessing import preprocess_data
+from spacy.lang.en import English
 
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 stop_words = set(stopwords.words('english'))
-porter = PorterStemmer()
+lemmatizer = nltk.WordNetLemmatizer()
+parser = English()
 
-def preprocess_text(text):
+def preprocess_text(text, language='english'):
     """
-    Cleans and preprocesses the input text by removing unwanted characters, converting to lowercase, 
-    tokenizing, removing stop words, and stemming.
+    Cleans and preprocesses the input text by removing unwanted characters, converting to lowercase,
+    tokenizing, removing stop words, and lemmatizing or stemming based on the language.
     """
     text = re.sub(r'http\S+', '', text)  # Remove URLs
     text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove non-alphabetic characters
     text = text.lower()  # Convert to lowercase
-    tokens = word_tokenize(text)  # Tokenize using NLTK's word_tokenize function
+    doc = parser(text)
+    if language == 'english':
+        tokens = [token.lemma_ if token.lemma_ != "-PRON-" else token.text for token in doc]
+    else:
+        tokens = [token.text for token in doc]
     tokens = [token for token in tokens if token not in stop_words]  # Remove stop words
-    stemmed_tokens = [porter.stem(token) for token in tokens]  # Stemming using PorterStemmer
-    return stemmed_tokens
+    if language == 'english':
+        tokens = [lemmatizer.lemmatize(token, get_wordnet_pos(token)) for token in tokens]  # Lemmatization
+    else:
+        porter = nltk.PorterStemmer()
+        tokens = [porter.stem(token) for token in tokens]  # Stemming using PorterStemmer
+    return tokens
 
-def preprocess_dataframe(df, text_column):
+def get_wordnet_pos(word):
+    """
+    Maps POS tag to first character used by WordNetLemmatizer.
+    """
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN)
+
+def preprocess_dataframe(df, text_column, language='english'):
     """
     Preprocesses the text in the specified column of the input dataframe.
     """
-    df[text_column] = df[text_column].apply(lambda x: preprocess_text(x))
+    df[text_column] = df[text_column].apply(lambda x: preprocess_text(x, language))
     return df
