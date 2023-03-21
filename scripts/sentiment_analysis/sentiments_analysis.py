bf6beec3df05052575a5edd4b7d0fb1632d9693f
TripVerse <122679016+Tripverseofficial@users.noreply.github.com>
2023-03-21 23:09:44 +0700

    Update sentiments_analysis.py

--- scripts/sentiment_analysis/sentiments_analysis.py
+++ scripts/sentiment_analysis/sentiments_analysis.py
@@ -1,30 +1,46 @@
 import pandas as pd
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from data.sentiments_analysis.news_scraper import scrape_news
 from data.sentiments_analysis.twitter_scraper import scrape_tweets
 from data.sentiments_analysis.telegram_scraper import scrape_messages
-from data.sentiments_analysis.preprocessing import preprocess_dataframe
-from data.sentiments_analysis.sentiment_analysis import analyze_sentiment
+from data.sentiments_analysis.preprocessing import preprocess_text
+from data.sentiments_analysis.sentiment_analysis import calculate_sentiment_scores
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-nltk.download('vader_lexicon')
-sid = SentimentIntensityAnalyzer()
+def perform_sentiment_analysis(news_url, twitter_keyword, twitter_limit, telegram_keyword, telegram_limit):
+    # Initialize VADER sentiment analyzer
+    sid = SentimentIntensityAnalyzer()
 
-def perform_sentiment_analysis(data):
-    data = preprocess_dataframe(data, 'text')
-    sentiment_scores = pd.DataFrame(columns=['date', 'text', 'compound'])
-    for index, row in data.iterrows():
-        text = row['text']
-        date = row['date']
-        compound = analyze_sentiment(sid, text)['compound']
-        sentiment_scores = sentiment_scores.append({'date': date, 'text': text, 'compound': compound}, ignore_index=True)
+    # Scrape data from all sources
+    news_data = scrape_news(news_url)
+    tweet_data = scrape_tweets(twitter_keyword, twitter_limit)
+    telegram_data = scrape_messages(telegram_keyword, telegram_limit)
+
+    # Concatenate dataframes
+    data = pd.concat([news_data, tweet_data, telegram_data])
+
+    # Preprocess data
+    data['text'] = data['text'].apply(preprocess_text)
+
+    # Calculate sentiment scores
+    sentiment_scores = calculate_sentiment_scores(data, sid)
+
+    # Sort sentiment scores by date
     sentiment_scores = sentiment_scores.sort_values('date')
+
     return sentiment_scores
 
-if __name__ == "__main__":
-    news_data = scrape_news('https://www.bbc.com/news')
-    tweet_data = scrape_tweets('Bitcoin', 100)
-    telegram_data = scrape_messages('Bitcoin', 100)
-    data = pd.concat([news_data, tweet_data, telegram_data])
-    sentiment_scores = perform_sentiment_analysis(data)
+def main():
+    # Example usage
+    news_url = 'https://www.bbc.com/news'
+    twitter_keyword = 'Bitcoin'
+    twitter_limit = 100
+    telegram_keyword = 'Bitcoin'
+    telegram_limit = 100
+
+    sentiment_scores = perform_sentiment_analysis(news_url, twitter_keyword, twitter_limit, telegram_keyword, telegram_limit)
+
+    # Print results
     print(sentiment_scores)
+
+if __name__ == "__main__":
+    main()
